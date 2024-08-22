ceae3b0f14de6e9f29900351ec8fd5475a9beb1e
Yousef26 <107435797+Yousef26@users.noreply.github.com>
2024-08-22 18:07:49 +0300

    public

---
+++ public/k.js
@@ -0,0 +1,6 @@
+console.log("jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj")
+var but1 =document.querySelector('.r5')
+but1.addEventListener('click', function() {
+  but1.style.backgroundColor = 'green'
+})
+console.log(but1)
