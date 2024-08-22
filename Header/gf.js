1c34316f792175c1458fcb3595ddeb93f7fc7ab9
Yousef26 <107435797+Yousef26@users.noreply.github.com>
2024-08-22 18:42:36 +0300

    Delete Header directory

--- Header/gf.js
+++
@@ -1,6 +0,0 @@
-var h=document.querySelector(".icon")
-console.log("jguihruhjrtiuhrgoyj76oiryu6iygnuliytgiurylhukrhgdfgdfkljgjndkgjdiojgoiuoeuhieurrhgeriuhg")
-var snnav= document.querySelector(".nav")
-h.addEventListener("click",function () {
-    h.classList.toggle("toin")
-})
