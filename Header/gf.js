ff7abfa42863f2d2ac0f90bb88eb5d532339e310
Yousef26 <107435797+Yousef26@users.noreply.github.com>
2024-08-22 18:22:06 +0300

    src

---
+++ Header/gf.js
@@ -0,0 +1,6 @@
+var h=document.querySelector(".icon")
+console.log("jguihruhjrtiuhrgoyj76oiryu6iygnuliytgiurylhukrhgdfgdfkljgjndkgjdiojgoiuoeuhieurrhgeriuhg")
+var snnav= document.querySelector(".nav")
+h.addEventListener("click",function () {
+    h.classList.toggle("toin")
+})
---
+++ Resume/jk.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function jk() {
+  return (
+console.log("ffffffffffffffffffffffffffffffff")
+  )
+}
+
+export default jk
