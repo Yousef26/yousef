2e245ba2454d82e1a65d2f2334b999d3715e17df
unknown <yousefkazem2@gmail.com>
2024-08-25 08:44:58 +0300

    add drinks

---
+++ src/app/drinks/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './drinks.css'
+import  lgo  from'../7436705_3562984.jpg'
+import Image from 'next/image'
+function drinks() {
+    return (
+        <div>
+            <div className="drinkindrink">
+                <div className="cold">
+                    <div className="coldheader">cold</div>
+                    <div className="coldcards">
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    </div>
+                </div>
+                <hr />
+                <div className="hot">
+                <div className="hotheader">hot</div>
+                    <div className="coldcards">
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default drinks
