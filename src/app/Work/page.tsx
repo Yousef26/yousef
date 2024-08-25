a843fb9c76f75817d9952c423233760d110a7e36
unknown <yousefkazem2@gmail.com>
2024-08-25 09:16:28 +0300

    modifi

--- src/app/Work/page.tsx
+++ src/app/Work/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 import React, { useState } from 'react'
 import "./work.css";
-import loo  from '/images/d1.jpg'
-import loo1 from '/images/d2.jpg'
-import loo2 from '/images/d1.jpg'
-import loo3 from '/images/dk.jpg'
+// import loo  from '/images/d1.jpg'
+// import loo1 from '/images/d2.jpg'
+// import loo2 from '/images/d1.jpg'
+// import loo3 from '/images/dk.jpg'
 import Link from 'next/link';
 
 
@@ -14,11 +14,11 @@ function Work() {
   let ar = ['React , Css , Js', 'Html , BootStrap , js', 'Html , TailWind , JS , PHP', 'Html , React-BootStrap , React']
   let name = ['E-commorce', 'youtube', 'movies', 'Games']
   let loren = [' ipsum elit. Doloremque recusandae nisi ipsam alia.', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia deleniti, non nam as', ' ipsum dolor sit aum elit. Doloremque recusandae nisimet, consectetur adipisicing elit. Officia deleniti, non nam as', 'Lorem ipsum dolor sit aum elit. Doloremque recusandae nisimet, consectetur adipisicing elit. Officia deleniti, non nam as']
-  let img = [loo, loo1, loo2, loo3]
+  // let img = [loo, loo1, loo2, loo3]
   const [lang, setlang] = useState(ar[0])
   const [nam, setanm] = useState(name[0])
   const [text, settext] = useState(loren[0])
-  const [i, seti] = useState(img[0])
+  // const [i, seti] = useState(img[0])
 
   return (
     <div>
@@ -37,10 +37,11 @@ function Work() {
             </div>
           </div>
           <div className="wtow">
-            <Image className='wttt' src={i} alt='' />
+            {/* <Image className='wttt' src={i} alt='' /> */}
+            <img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" />
             <div className="wmove">
-              <div className="wright" onClick={() => { num <= 0 ? num = num : --num; setlang(ar[num]); setanm(name[num]); settext(loren[num]);seti(img[num])  }}></div>
-              <div className="wleft" onClick={() => { num >= 3 ? num = num : ++num; setlang(ar[num]); setanm(name[num]); settext(loren[num]);seti(img[num]) }}></div>
+              <div className="wright" onClick={() => { num <= 0 ? num = num : --num; setlang(ar[num]); setanm(name[num]); settext(loren[num])/** ;seti(img[num]) */ }}></div>
+              <div className="wleft" onClick={() => { num >= 3 ? num = num : ++num; setlang(ar[num]); setanm(name[num]); settext(loren[num])/** ;seti(img[num]) */ }}></div>
             </div>
           </div>
         </div>
--- src/app/drinks/page.tsx
+++ src/app/drinks/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './drinks.css'
-import  lgo  from'/images/d1.jpg'
-import Image from 'next/image'
+// import  lgo  from'/images/d1.jpg'
+// import Image from 'next/image'
 function drinks() {
     return (
         <div>
@@ -9,36 +9,36 @@ function drinks() {
                 <div className="cold">
                     <div className="coldheader">cold</div>
                     <div className="coldcards">
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
                     </div>
                 </div>
                 <hr />
                 <div className="hot">
                 <div className="hotheader">hot</div>
                     <div className="coldcards">
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
-                    <div className="card"><Image src={lgo} alt="" className='drinksimg'/><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
+                    <div className="card"><img src="https://d1ralsognjng37.cloudfront.net/c9999e66-7040-4890-bf72-c5ee49ff0a2a.jpeg" alt="lunch" /><div className="drinksprice">30$</div><button className='drinkdetails'>details </button></div>
                     </div>
                 </div>
             </div>
